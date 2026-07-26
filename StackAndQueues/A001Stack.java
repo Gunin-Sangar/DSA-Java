5056751a6c4e889ed690dc3b037474c85cfdaa95
Gunin <gunin0301@gmail.com>
2026-07-26 21:45:24 +0530

    Finally added ArrayList (that to after LinkedList xd) with stack and queue

---
+++ StackAndQueues/A001Stack.java
@@ -0,0 +1,40 @@
+package StackAndQueues;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.ArrayList;
+public class A001Stack {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(12);
+        stack.push(23);
+        stack.push(34);
+        stack.push(45);
+        stack.push(56);
+        stack.push(67);
+
+        int num = stack.pop();
+        System.out.println("Stack            : " + stack);
+        System.out.println("Stack.getClass() : " + stack.getClass());
+        System.out.println("num              : " + num);
+        System.out.println("int.class        : " + int.class);
+        System.out.println();
+        // Primitive data types (int, double, char, boolean, etc.) are not objects,
+        // so they don't have the getClass() method. Use <primitive>.class instead.
+
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(12);
+        queue.add(23);
+        queue.add(34);
+        queue.add(45);
+        queue.add(56);
+        queue.add(67);
+
+        int qnum = queue.peek();
+        System.out.println("queue            : " + queue);
+        System.out.println("queue.getClass() : " + queue.getClass());
+        System.out.println("qnum              : " + qnum);
+        System.out.println("int.class        : " + int.class);
+    }
+}
