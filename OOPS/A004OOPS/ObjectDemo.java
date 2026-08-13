70a68d0700737379a8d460766ab7f1ea784f68b3
Gunin <gunin0301@gmail.com>
2026-08-13 22:27:24 +0530

    Object class [open ObjectDemo.java]

---
+++ OOPS/A004OOPS/ObjectDemo.java
@@ -0,0 +1,35 @@
+package OOPS.A004OOPS;
+
+// Press alt + insert
+// navigate to override methods
+// see all methods provided by the Object class [all classes are subclass of Object class]
+public class ObjectDemo {
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    public ObjectDemo() {
+        super();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
